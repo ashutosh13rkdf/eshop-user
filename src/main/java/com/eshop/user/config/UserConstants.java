ee5c1cdd27df5f55be9385343ca7b6daa07aa363
ashutosh13rkdf <ashutosh13rkdf@gmail.com>
2022-09-11 12:16:04 +0530

    Update UserConstants.java

--- src/main/java/com/eshop/user/config/UserConstants.java
+++ src/main/java/com/eshop/user/config/UserConstants.java
@@ -5,4 +5,5 @@ public class UserConstants {
     public static final Integer USER_ALREADY_EXIST_ERR_CODE = 1405;
     public static final Integer INTERNAL_SERVER_ERROR = 1500;
     public static final Integer GENERIC_EXCEPTION = 1600;
+    public static final Integer IO_EXCEPTION = 1601;
 }
