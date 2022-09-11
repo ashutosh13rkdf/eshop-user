b9f304ad4642ac8d69172ce2ab08701b10c7c966
ASHUTOSH KUMAR SHRIVASTAVA <ashutosh13rkdf@gmail.com>
2022-09-11 11:56:03 +0530

    added new constant

--- src/main/java/com/eshop/user/config/UserConstants.java
+++ src/main/java/com/eshop/user/config/UserConstants.java
@@ -3,4 +3,5 @@ package com.eshop.user.config;
 public class UserConstants {
     public static final Integer USER_NOT_FOUND_ERR_CODE = 1404;
     public static final Integer USER_ALREADY_EXIST_ERR_CODE = 1405;
+    public static final Integer INTERNAL_SERVER_ERROR = 1500;
 }
